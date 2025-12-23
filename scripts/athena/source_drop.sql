aba1ebe997fdca5582b2fc61f3ecaac4040e74a6
vuhuusy <vuhuusytft1@gmail.com>
2025-12-23 21:14:45 +0700

    refactor(athena): simplify database drop by removing individual table drops

--- scripts/athena/source_drop.sql
+++ scripts/athena/source_drop.sql
@@ -1,12 +1,1 @@
--- Drops Athena objects for Grocery Sales Database
--- Order matters: drop tables first, then drop the database.
-
-DROP TABLE IF EXISTS mdw_raw.sales;
-DROP TABLE IF EXISTS mdw_raw.products;
-DROP TABLE IF EXISTS mdw_raw.employees;
-DROP TABLE IF EXISTS mdw_raw.customers;
-DROP TABLE IF EXISTS mdw_raw.cities;
-DROP TABLE IF EXISTS mdw_raw.countries;
-DROP TABLE IF EXISTS mdw_raw.categories;
-
-DROP DATABASE IF EXISTS mdw_raw;
+DROP DATABASE IF EXISTS mdw_raw CASCADE;
