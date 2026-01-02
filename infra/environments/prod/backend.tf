048225154c96357750c525c3071e0fd8e235e903
vuhuusy <vuhuusytft1@gmail.com>
2026-01-03 00:01:45 +0700

    fix: Update backend configuration comments and replace DynamoDB locking with native S3 state locking

--- infra/environments/prod/backend.tf
+++ infra/environments/prod/backend.tf
@@ -2,7 +2,7 @@
 # Terraform Backend Configuration - Production Environment
 ################################################################################
 #
-# State is stored in S3 with DynamoDB locking for team collaboration.
+# State is stored in S3 with native S3 state locking for team collaboration.
 # The backend bucket must be created before running terraform init.
 #
 # To create backend resources, run:
@@ -12,10 +12,10 @@
 
 terraform {
   backend "s3" {
-    bucket         = "mdw-prod-us-west-2-tfstate"
-    key            = "infrastructure/terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
-    dynamodb_table = "mdw-prod-us-west-2-tfstate-lock"
+    bucket       = "mdw-prod-us-west-2-tfstate"
+    key          = "infrastructure/terraform.tfstate"
+    region       = "us-west-2"
+    encrypt      = true
+    use_lockfile = true
   }
 }
